cacc29f426c312745508409cdae165f9ee69030c
neirinzaralwin <zaralwinneirin@gmail.com>
2023-09-23 21:32:51 +0630

    add brand

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,8 @@ from ecommerce.product import views
 
 router = DefaultRouter()
 router.register(r"category", views.CategoryViewSet)
+router.register(r"brand", views.BrandViewSet)
+router.register(r"product", views.ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
